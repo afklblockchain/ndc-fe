4fdff48cb0772c2f9d666eb22474f9ff8b13a718
Deniz Vahaboglu <deniz.vahaboglu@klm.com>
2018-10-24 18:23:44 +0200

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('afkl-root h1')).getText();
+  }
+}
